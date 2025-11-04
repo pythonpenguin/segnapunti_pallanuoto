4b2090b3fc4fd86a427eb7f287ef5c4e57d99d67
pythonpenguin <davide.soresinetti@gmail.com>
2025-11-04 16:17:20 +0100

    https://github.com/pythonpenguin/segnapunti_pallanuoto/issues/16#issue-3579375722

--- rasp5/src/mappa_input.py
+++ rasp5/src/mappa_input.py
@@ -27,13 +27,13 @@ class MappaInput:
     GPIO_MAPPING = {
         23: ("goal_casa_piu", "RISING_EDGE"),           # Bottone 1
         17: ("goal_casa_meno", "RISING_EDGE"),          # Bottone 2
-        24: (None, "RISING_EDGE"),                       # Bottone 3 - non mappato
+        24: ("next_period", "RISING_EDGE"),                       # Bottone 3 - non mappato
         6: ("reset_possesso_palla", "RISING_EDGE"),      # Bottone 4 - 28 secondi
         13: ("start_stop", "BOTH_EDGES"),                # Bottone 5 - start/stop
         5: ("set_tempo_aggiuntivo", "RISING_EDGE"),      # Bottone 6 - 18 secondi
         18: ("goal_tasferta_piu", "RISING_EDGE"),       # Bottone 7
         27: ("goal_tasferta_meno", "RISING_EDGE"),      # Bottone 8
-        15: (None, "RISING_EDGE"),                       # Bottone 9 - non mappato
+        15: ("prev_period", "RISING_EDGE"),                       # Bottone 9 - non mappato
         22: (None, "RISING_EDGE"),                       # Bottone 10 - non mappato
         14: (None, "RISING_EDGE"),                       # Bottone 11 - non mappato
         4: (None, "RISING_EDGE"),                        # Bottone 12 - non mappato
